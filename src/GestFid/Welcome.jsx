bbc494e2af46a7bfbebd216ac9999829d41b4233
MicheleLaMacchia <66997236+MicheleLaMacchia@users.noreply.github.com>
2021-01-19 16:42:28 +0100

    first commit bis

--- src/GestFid/Welcome.jsx
+++ src/GestFid/Welcome.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom"
+
 const WelcomeComponent = (props) => {
   return ( 
   <div className="WelcomeComponent">
     <h1>Benvenuti in GestFid</h1>
     <h2>Ciao, {props.match.params.userid}</h2>
+    <h3>clicca <Link to='/clienti'>qui</Link> per vedere i clienti disponibili</h3>
   </div> );
 }
  
